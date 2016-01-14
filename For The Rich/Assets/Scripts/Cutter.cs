71916075533b6d9c2fc5ba7c0a49d0171304916f
Sergii Mo <yegresom@gmail.com>
2016-01-14 21:54:10 +0100

    Cutter script

---
+++ For The Rich/Assets/Scripts/Cutter.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Cutter : MonoBehaviour
+{
+	
+	public RectTransform touchField;
+	private bool reloaded = false;
+
+	void Update ()
+	{	 
+		if (reloaded && BouthTouchesOnField ()) {
+			reloaded = false;
+			Cut ();
+		}
+
+		if (BouthTouchesNotOnField ()) {
+			reloaded = true;
+		}
+	}
+
+	void Cut ()
+	{
+		//тут посчитать середину между тачями
+		//сконвертировать с координат тачскрина в систему объектов
+		//и сделать что-то типа: money.Cut(cutPoint);
+	}
+
+	bool BouthTouchesOnField ()
+	{
+		return Input.touchCount == 2 && IsTouchAtField (Input.touches [0])
+			&& IsTouchAtField (Input.touches [1]);
+	}
+
+	bool BouthTouchesNotOnField ()
+	{
+		return Input.touchCount == 2 && !IsTouchAtField (Input.touches [0])
+			&& !IsTouchAtField (Input.touches [1]);
+	}
+
+	bool IsTouchAtField (Touch touch)
+	{
+		return RectTransformUtility.RectangleContainsScreenPoint (touchField, touch.position);
+	}
+}
