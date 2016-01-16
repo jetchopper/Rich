34f155b94e16b6a22b210ec50b42b99bfd1539c5
jetchopper <mixutk@gmail.com>
2016-01-16 02:08:01 +0200

    New features
    Added concept GUI
    Smooth flight and disappearance added

--- For The Rich/Assets/Scripts/CubeMove.cs
+++ For The Rich/Assets/Scripts/CubeMove.cs
@@ -7,18 +7,25 @@ public class CubeMove : MonoBehaviour {
 
 	bool rotate;
 	Vector3 randomRot;
+	float timer;
 
 	// Update is called once per frame
 	void Update () {
 		transform.Translate(Vector3.left * Time.deltaTime * speed);
 		if (rotate){
+			timer += Time.deltaTime;
 			transform.Rotate(randomRot);
+			if (timer > 18f){
+				Debug.Log("changing color");
+				transform.GetChild(0).GetComponent<Renderer>().material.color -= Color.black * 0.02f;
+			}
 		}
 	}
 
 	public void SelfDestructor(){
 		rotate = true;
-		randomRot = new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f));
-		Destroy(gameObject, 10f);
+		timer = 0f;
+		randomRot = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 0f), Random.Range(-1f, 1f));
+		Destroy(gameObject, 20f);
 	}
 }
