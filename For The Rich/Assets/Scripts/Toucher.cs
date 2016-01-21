4105eb5b47de9c9ca59522f412ec13dc25925167
jetchopper <mixutk@gmail.com>
2016-01-22 01:30:43 +0200

    NEW features

--- For The Rich/Assets/Scripts/Toucher.cs
+++ For The Rich/Assets/Scripts/Toucher.cs
@@ -7,22 +7,15 @@ public class Toucher : MonoBehaviour  {
 	public BladeRotate1 bladeR;
 	public float scissorsClosePercent;
 
-	Vector2 initTouchPos;
 	float gainedPercent;
-
-	void Awake(){
-		gainedPercent = 0f;
-	}
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.touchCount > 0){
-			if (Input.GetTouch(0).phase == TouchPhase.Began){
-				initTouchPos = Input.GetTouch(0).position;
-			}
-			if (Input.GetTouch(0).phase == TouchPhase.Moved && gainedPercent < scissorsClosePercent){
-				gainedPercent += Vector2.Distance(initTouchPos, 
-				                                         Input.GetTouch(0).deltaPosition) * Time.deltaTime * 10f;
+			if (Input.GetTouch(0).phase == TouchPhase.Began 
+			    || Input.GetTouch(0).phase == TouchPhase.Stationary 
+			    && gainedPercent < scissorsClosePercent){
+				gainedPercent += 50;				
 				bladeL.StartRotate(gainedPercent);
 				bladeR.StartRotate(gainedPercent);
 			}
